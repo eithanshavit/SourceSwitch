b488db9ed819cb1312c762eeea97bd0206f3a447
Eithan Shavit <oogiyot@gmail.com>
2011-10-29 18:06:25 -0700

    git init

---
+++ sourceswitch.pl
@@ -0,0 +1,94 @@
+use strict;
+use warnings;
+use Cwd;
+use File::Copy;
+
+# edit with your paths
+my $user = 'eithans';
+my $view = 'android_platform_178';
+
+my $destination = "\\\\marvell\\israel\\LinuxDev\\swdev_platdev\\$user\\$view\\src\\kernel\\linux-2.6.32";
+# my $destination = "\\\\Pt-dt0033\\code\\tboot\\obm_v_122\\OBM";
+my $root = substr($ARGV[0],1,-1);
+
+
+
+
+print "\n";
+print "Source Switcher\n";
+print "---------------\n";
+print "Source is :      $root\n";
+print "Destination is : $destination\ \n";
+print "\n";
+print "OK? (y=copy files, l=list files, n=abort): ";
+my $ask = <STDIN>;
+chomp $ask;
+print "\n";
+my $islist = "list";
+if ($ask eq "y")
+{
+	$islist = "nolist";
+	rec($root,"");
+	print "---------------\n";
+	print "---------------\n";
+	print "Success!\n";
+	sleep 2;
+}
+if ($ask eq "l")
+{
+	# listing on
+	$islist = "list";
+	rec($root,"");
+	print "---------------\n";
+	print "---------------\n";
+	print "Folder listed. Press any key to continue...\n";
+	my $wait = <STDIN>;
+}
+if (($ask ne "l") && ($ask ne "y"))
+{
+	print "---------------\n";
+	print "---------------\n";
+	print "Aborted\n";
+	sleep 2;
+}
+
+sub rec{
+	my $source = shift;
+	my $relative_dir = shift;
+	opendir (DIR, $source) or die "Cannot open the source folder for reading:$!\n"; 
+	my @files = sort grep {!/^\.{1,2}$/} readdir(DIR); 
+	closedir (DIR);
+	
+	for (@files){
+	{ 
+		my $full_path = "$source\\$_"; 
+		my $new_file = "$destination$relative_dir\\$_";
+		if (-d $full_path)
+		# if &_ is dir
+		{ 
+			# print("  dir:   $full_path\n");
+			# print("create dir -> $new_file\n");			
+			# mkdir "$new_file" or die "Folder Cannot be created";
+			rec($full_path,"$relative_dir\\$_");
+			
+		}
+		else
+		# if &_ is file
+		{ 
+			# print("  copy to :  -> $new_file\n"); 
+			if ($islist eq "list")
+			{
+				print("file :     $relative_dir\\$_\n"); 
+				# print("$new_file\n"); 
+			}
+			else 
+			{
+				print("$relative_dir\\$_\n"); 
+				# print("$full_path, $new_file\n"); 
+				# sleep 5;
+				copy($full_path, $new_file) or die "File cannot be copied - check if dir exists. ";
+			}
+		} 
+	} 
+}
+}
